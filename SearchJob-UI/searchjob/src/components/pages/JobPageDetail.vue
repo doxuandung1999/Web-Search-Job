e3224b9b65267a00a1315c56818373ba6d2998fb
sy phung <syphung97@gmail.com>
2021-12-04 00:23:55 +0700

    vẽ detail

--- SearchJob-UI/searchjob/src/components/pages/JobPageDetail.vue
+++ SearchJob-UI/searchjob/src/components/pages/JobPageDetail.vue
@@ -1,11 +1,175 @@
 <template>
-  <v-container>
-    <v-row class="d-flex align-center">
-      <div class="box-white box-detail-job">
-        <div class="logo"></div>
-      </div>
-    </v-row>
-  </v-container>
+  <div class="w-full wrap">
+    <v-container>
+      <v-row class="w-full">
+        <div class="box-white flex-m m-t-32 box-detail-job w-full">
+          <div class="logo">
+            <img
+              src="https://cdn.topcv.vn/80/company_logos/cong-ty-co-phan-propertyguru-viet-nam-5f39fa780ef62.jpg"
+              alt=""
+              srcset=""
+            />
+          </div>
+          <div class="box-info-job flex-grow-1">
+            <h1 class="job-title text-highlight bold">
+              Nhân Viên Tư Vấn - Website Thương Mại Điện Tử
+            </h1>
+            <div class="company-title">
+              <a
+                href="https://www.topcv.vn/cong-ty/cong-ty-co-phan-propertyguru-viet-nam/158.html"
+                class="text-dark-blue fs-18 bold"
+                >Công ty Cổ phần PropertyGuru Việt Nam</a
+              >
+            </div>
+            <div class="job-deadline">
+              <v-icon class="fs-20">mdi-clock-outline</v-icon>
+              <span class="m-t-4 m-l-4">Hạn nộp hồ sơ: 17/12/2021</span>
+            </div>
+          </div>
+          <div class="btn-group h-full">
+            <v-btn block large class="ma-2" color="success">
+              <v-icon left dark class="m-r-16"> mdi-send </v-icon>
+              <span style="font-weight: 500">Ứng tuyển ngay</span>
+            </v-btn>
+            <v-btn block large outlined class="ma-2 m-t-32" color="success">
+              <v-icon left dark class="m-r-16"> mdi-heart-outline </v-icon>
+              <span style="font-weight: 500">Lưu tin</span>
+            </v-btn>
+          </div>
+        </div>
+      </v-row>
+      <v-row class="w-full">
+        <div class="box-white flex-t m-t-32 box-detail-job w-full">
+          <v-col style="padding: 0 !important" cols="8">
+            <h1 class="box-title">Chi tiết tin tuyển dụng</h1>
+            <div class="box-info">
+              <p>Thông tin chung</p>
+              <div class="box-main">
+                <div class="box-item flex-m m-t-16">
+                  <img
+                    src="https://www.topcv.vn/v4/image/job-detail/icon/1.svg"
+                    alt=""
+                  />
+                  <div class="m-l-16">
+                    <strong>Mức lương </strong> <br />
+                    <span>
+                      <a class="text-highlight"> Đăng nhập để xem mức lương</a>
+                    </span>
+                  </div>
+                </div>
+                <div class="box-item flex-m m-t-16">
+                  <img
+                    src="https://www.topcv.vn/v4/image/job-detail/icon/1.svg"
+                    alt=""
+                  />
+                  <div class="m-l-16">
+                    <strong>Mức lương </strong> <br />
+                    <span>
+                      <a class="text-highlight"> Đăng nhập để xem mức lương</a>
+                    </span>
+                  </div>
+                </div>
+                <div class="box-item flex-m m-t-16">
+                  <img
+                    src="https://www.topcv.vn/v4/image/job-detail/icon/2.svg"
+                    alt=""
+                  />
+                  <div class="m-l-16">
+                    <strong>Mức lương </strong> <br />
+                    <span>
+                      <a class="text-highlight"> Đăng nhập để xem mức lương</a>
+                    </span>
+                  </div>
+                </div>
+                <div class="box-item flex-m m-t-16">
+                  <img
+                    src="https://www.topcv.vn/v4/image/job-detail/icon/3.svg"
+                    alt=""
+                  />
+                  <div class="m-l-16">
+                    <strong>Mức lương </strong> <br />
+                    <span>
+                      <a class="text-highlight"> Đăng nhập để xem mức lương</a>
+                    </span>
+                  </div>
+                </div>
+                <div class="box-item flex-m m-t-16">
+                  <img
+                    src="https://www.topcv.vn/v4/image/job-detail/icon/4.svg"
+                    alt=""
+                  />
+                  <div class="m-l-16">
+                    <strong>Mức lương </strong> <br />
+                    <span>
+                      <a class="text-highlight"> Đăng nhập để xem mức lương</a>
+                    </span>
+                  </div>
+                </div>
+                <div class="box-item flex-m m-t-16">
+                  <img
+                    src="https://www.topcv.vn/v4/image/job-detail/icon/5.svg"
+                    alt=""
+                  />
+                  <div class="m-l-16">
+                    <strong>Mức lương </strong> <br />
+                    <span>
+                      <a class="text-highlight"> Đăng nhập để xem mức lương</a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="box-address">
+              <p>Địa điểm làm việc</p>
+              <div>
+                <div class="text-dark-gray">
+                  - Khu vực: Hà Nội, Hồ Chí Minh, Bình Dương, Đồng Nai, Đà Nẵng,
+                  Bà Rịa-Vũng Tàu, TP Nha Trang, Hải Phòng
+                </div>
+                <div class="text-dark-gray" style="margin-bottom: 10px">
+                  - Tầng 31, Keangnam Hanoi Landmark, Phạm Hùng, Nam Từ Liêm, Hà
+                  Nội, các tỉnh thành trên cả nước
+                </div>
+              </div>
+            </div>
+
+            <div class="content">Content here</div>
+
+            <div class="box-how-to-apply">
+              <h2>Cách thức ứng tuyển</h2>
+              <p class="m-t-8">
+                Ứng viên nộp hồ sơ trực tuyến bằng cách bấm
+                <strong class="text-highlight">Ứng tuyển ngay</strong> dưới đây.
+              </p>
+              <div class="flex-m">
+                <v-btn large class="" color="success">
+                  <span style="font-weight: 500">Ứng tuyển ngay</span>
+                </v-btn>
+                <v-btn large outlined class="m-l-8" color="success">
+                  <span style="font-weight: 500">Lưu tin</span>
+                </v-btn>
+              </div>
+            </div>
+          </v-col>
+          <v-col cols="4">
+            <div class="box-share-job">
+              <h1>Chia sẻ tin tuyển dụng</h1>
+              <p>Sao chép đường dẫn</p>
+              <div class="box-copy">
+                <div class="url-copy">
+                  https://www.topcv.vn/viec-lam/nhan-vien-tu-van-website-thuong-mai-dien-tu/515383.html
+                </div>
+                <div class="btn-copy flex-c-m hov-pointer">
+                  <v-icon color="#00b14f">mdi-animation</v-icon>
+                </div>
+              </div>
+              
+            </div>
+          </v-col>
+        </div>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -15,11 +179,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+a {
+  text-decoration: none;
+  color: black;
+}
+.wrap {
+  background-color: #f0f0f0;
+}
 .box-white {
   background: white;
   border-radius: 3px;
-  display: flex;
-  align-items: center;
+
   padding: 16px;
   .logo {
     width: 110px;
@@ -32,6 +202,107 @@ export default {
     padding: 4px;
     background: #fff;
     justify-content: center;
+    img {
+      max-height: 100% !important;
+      -o-object-fit: contain;
+      object-fit: contain;
+      width: 75%;
+    }
   }
+  .box-info-job {
+    flex-grow: 1;
+    padding: 16px;
+    border-radius: 3px;
+    .job-title {
+      font-size: 22px;
+      margin-bottom: 16px;
+      line-height: 29px;
+    }
+    .company-title {
+      margin-bottom: 8px;
+    }
+  }
+  .btn-group {
+    padding: 0 16px;
+  }
+
+  .box-title {
+    color: #333;
+    font-size: 22px;
+    padding-left: 12px;
+    border-left: 7px solid #00b14f;
+    font-weight: 700;
+    margin: 0 0 16px;
+  }
+  .box-info {
+    background: rgba(0, 177, 79, 0.050980392156862744);
+    padding: 16px 16px 0;
+    margin-bottom: 8px;
+    border-radius: 3px;
+    p {
+      color: #333;
+      -webkit-text-decoration-line: underline;
+      text-decoration-line: underline;
+      font-weight: 700;
+      margin-bottom: 16px;
+    }
+    .box-main {
+      display: grid;
+      grid-template-columns: 50% 50%;
+      .box-item {
+        display: flex;
+        margin-bottom: 16px;
+      }
+    }
+  }
+  .box-address {
+    background: rgba(0, 177, 79, 0.050980392156862744);
+    padding: 16px;
+    border-radius: 3px;
+    p {
+      color: #333;
+      -webkit-text-decoration-line: underline;
+      text-decoration-line: underline;
+      font-weight: 700;
+      margin-bottom: 16px;
+    }
+  }
+  .box-how-to-apply {
+    h2 {
+      font-size: 18px;
+      color: #000;
+      font-weight: 700;
+    }
+  }
+  .box-share-job {
+    border: 1px solid #eee;
+    padding: 16px;
+    margin-bottom: 24px;
+    .box-copy {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 16px;
+      .url-copy {
+        white-space: nowrap !important;
+        overflow: hidden !important;
+        text-overflow: ellipsis !important;
+        background: #fafafa;
+        border-radius: 5px;
+        padding: 10px 14px;
+        width: 86%;
+      }
+      .btn-copy {
+        width: 11%;
+        background: #f2fbf6;
+        border-radius: 5px;
+        width: 40px;
+        height: 40px;
+        border: none;
+      }
+    }
+  }
+}
+.text-highlight {
+  color: #00b14f;
 }
 </style>
